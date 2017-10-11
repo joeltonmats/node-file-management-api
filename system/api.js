2cd779e57859539043c615f644d280a1e98f6752
joeltonmats <joeltonmatos@gmail.com>
2017-10-11 14:06:20 -0300

    Update api.js
    Add last modification generate files.

--- system/api.js
+++ system/api.js
@@ -47,14 +47,43 @@ exports.getContentFile = (dirOrigin, callback) => {
     })
 };
 
+/*
+
+node index.js -c c:/NEW-FILES/start ///172.24.21.198/shared/files
+
+
+MAX RANGE TO GENERATE FILES
+
+0 - 5000000                 done
+5000000 - 10000000          done
+10000000 - 15000000         done
+15000000 - 20000000         done 
+20000000 - 25000000         done
+25000000 - 30000000         done
+30000000 - 35000000         done
+35000000 - 40000000         done
+40000000 - 45000000         done 
+45000000 - 50000000         done
+50000000 - 55000000         done
+55000000 - 60000000         done
+60000000 - 65000000         done
+65000000 - 70000000         done
+70000000 - 75000000         done
+75000000 - 80000000         done
+80000000 - 85000000         done
+85000000 - 90000000         done
+90000000 - 95000000         done
+95000000 - 100000000        done
+
+*/
+
+
 exports.creatNewFileWithIncrementalContent = (dirDestiny, nameFile, content, incrementaNumberMore, callback) => {
 
     let lastLine = content[content.length - 2];
     const lastLineList = lastLine.split(';');
-    //let fieldSequential1 = Number(lastLineList[lastLineList.length - 3]);
-    //fieldSequential1 = 5000000;
-
-    for (let j = 99000000; j <= 99000000; j = j + 1000000) {
+    
+    for (let j = 0; j < 1000000; j = j + incrementaNumberMore) {
         const arrayNewItems = [];
         for (let i = 1; i <= incrementaNumberMore; i++) {
             let item = lastLineList.slice();
@@ -65,20 +94,14 @@ exports.creatNewFileWithIncrementalContent = (dirDestiny, nameFile, content, inc
             arrayNewItems.push(item);
         }
 
-        /* content = content.toString();
-        content = content.concat(arrayNewItems.toString());
-        content += '|';
-        content = content.replace(/,/g, '|');
-        fs.appendFile(dirDestiny + '/' + 'ONE-MILION.txt', content, 'utf8', (err) => {  */
-
         content = arrayNewItems.toString().trim();
         content += '|';
         content = content.replace(/,/g, '|');
 
-        fs.appendFile(dirDestiny + '/' + j + '-' + nameFile, content, 'utf8', (err) => {
+        fs.writeFile(dirDestiny + '/' + (j + incrementaNumberMore)  + '.TXT', content, 'utf8', (err) => {
             if (err) callback(err, null);
 
-            callback(null, 'sucesso!!!');
+            callback(null, (j + incrementaNumberMore) + '.TXT ' + 'sucesso!!!');
 
         })
 
